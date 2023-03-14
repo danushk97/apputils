dd7ab6f8b09712d97f1fc3fb56c67fc734ea610c
danushk97 <danushk97@gmail.com>
2023-03-14 16:01:43 +0530

    set default value for is_active prop in BaseModel class

--- src/appscommon/domain/models/basemodel.py
+++ src/appscommon/domain/models/basemodel.py
@@ -6,10 +6,11 @@ class BaseModel:
         created_by: int,
         created_at: datetime,
         last_updated_by: int,
-        last_updated_at: datetime
+        last_updated_at: datetime,
+        is_active: bool = True
     ) -> None:
         self.created_by = created_by
         self.created_at = created_at
         self.last_updated_by = last_updated_by
         self.last_updated_at = last_updated_at
-        self.is_active = True
+        self.is_active = is_active
